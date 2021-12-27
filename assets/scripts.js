d0ee43466bc21dc54be6549c91754052902d6dd1
incogNic0 <incognico.dev@gmail.com>
2021-12-27 17:30:18 -0600

    add time calculations

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,5 @@
 const calcBtn = document.querySelector('.calc-btn');
-const timesIn = document.querySelectorAll('.time-in')
-const timesOut = document.querySelectorAll('.time-out');
+
 const hrsWorked = document.querySelectorAll('.hours-worked');
 
 
@@ -11,27 +10,38 @@ window.addEventListener('keypress', evt => {
 calcBtn.addEventListener('click', handleCalculate);
 
 function handleCalculate(evt) {
-  clearErrs();
-  const inputErrs = validInOut(timesIn, timesOut);
+  const timesIn = document.querySelectorAll('.time-in')
+  const timesOut = document.querySelectorAll('.time-out');
+  const convertedIns = convertInputs(timesIn, 'in');
+  const convertedOuts = convertInputs(timesOut, 'out');
+  let totalHrs = 0;
 
-  if(inputErrs.messages.length) {
-    notifyErrors(inputErrs);
+  clearErrs();
+  const inputErrs = validInOut(convertedIns, convertedOuts);
+
+  if(inputErrs) {
+    return notifyErrors(inputErrs);
   }
 
-  const convertedIns = Array.from(timesIn)
-    .map(inTime => convertTime(inTime.value));
-
-  const convertedOuts = Array.from(timesOut)
-    .map(outTime => convertTime(outTime.value));
+  calculateHrs(convertedIns, convertedOuts);
 
 }
 
-function convertTime(str) {
-  if(!str) return null;
-  const timeArr = str.split(':');
-  return timeArr.map(numStr => Number(numStr))
+// CLEAR ALL ERROR NOTIFICATIONS
+function clearErrs() {
+  const errMsgs = document.querySelector('.error-messages');
+  const missingInputs = document.querySelectorAll('.missing-invalid');
+  for(const elem of missingInputs) {
+    elem.classList.remove('missing-invalid');
+  }
+  while(errMsgs.firstChild) {
+    errMsgs.removeChild(errMsgs.firstChild);
+  }
+  errMsgs.classList.remove('show-errors')
+  window.scrollTo(0,0);
 }
 
+// VALIDATE TIME INPUTS
 function validInOut(inTimes, outTimes) {
   const days = [
     'Sunday',
@@ -43,35 +53,69 @@ function validInOut(inTimes, outTimes) {
     'Saturday'
   ]
   const errs = {
-    inIndexs: [],
-    outIndexs: [],
+    ins: [],
+    outs: [],
     messages: []
   }
+  let isErrors;
 
-  for(let i=0; i<inTimes.length; i++) {
-    const day = days[Math.floor(i / 2)];
-    if(inTimes[i].value && !outTimes[i].value) {
-      errs.outIndexs.push(i);
+  for(let i=0; i<14; i+=2) {
+    const day = days[ i / 2 ];
+
+    if (inTimes[i] && !outTimes[i]) {
+      errs.outs.push(i);
       errs.messages.push(`'Time Out' missing for ${day}`);
     }
-    if(outTimes[i].value && !inTimes[i].value) {
-      errs.inIndexs.push(i);
+    if (inTimes[i+1] && !outTimes[i+1]) {
+      errs.outs.push(i+1);
+      errs.messages.push(`'Time Out' missing for ${day}`);
+    }
+ 
+    if (outTimes[i] && !inTimes[i]) {
+      errs.ins.push(i);
       errs.messages.push(`'Time In' missing for ${day}`);
+    }
+
+    if (outTimes[i+1] && !inTimes[i+1]) {
+      errs.ins.push(i+1);
+      errs.messages.push(`'Time In' missing for ${day}`);
+    } 
+
+    if( inTimes[i] && outTimes[i]) {
+      if(inTimes[i].hour > outTimes[i].hour) {
+        errs.outs.push(i);
+        errs.messages.push(`Invalid 'Time Out' for ${day}`);
+      }
+    }
+    
+    if(inTimes[i+1] && outTimes[i+1]) {
+      if (inTimes[i+1].hour < outTimes[i].hour) {
+        errs.ins.push(i+1);
+        errs.messages.push(`Invalid 'Time In' for ${day}`);
+      }
+      if(inTimes[i+1].hour > outTimes[i+1].hour) {
+        errs.outs.push(i+1);
+        errs.messages.push(`Invalid 'Time Out' for ${day}`);
+      }
     }
   }
 
-  return errs;
+  return errs.messages.length ? errs : false;
 }
 
+// HIGHLIGHT INVALID/MISSING INPUTS
 function notifyErrors(errs) {
-  errs.inIndexs
-    .forEach( i => timesIn[i].classList.add('missing-required'));
-  errs.outIndexs
-    .forEach( i => timesOut[i].classList.add('missing-required'));
+  const timesIn = document.querySelectorAll('.time-in');
+  const timesOut = document.querySelectorAll('.time-out'); 
+  errs.ins
+    .forEach( i => timesIn[i].classList.add('missing-invalid'));
+  errs.outs
+    .forEach( i => timesOut[i].classList.add('missing-invalid'));
 
   errs.messages.forEach( msg => showErr(msg) )
 }
 
+// DISPLAY ERROR MESSAGES
 function showErr(msg) {
   const errMsgs = document.querySelector('.error-messages');
   const node = document.createElement('P');
@@ -81,15 +125,83 @@ function showErr(msg) {
   errMsgs.classList.add('show-errors');
 }
 
-function clearErrs() {
-  const errMsgs = document.querySelector('.error-messages');
-  const missingInputs = document.querySelectorAll('.missing-required');
-  for(const elem of missingInputs) {
-    elem.classList.remove('missing-required');
+
+// CONVERT TIME INPUTS
+function convertInputs(timeInputs, timeType = 'in') {
+  const plusDays = document.querySelectorAll('.plus-day-checkbox');
+  const output = [];
+  for (let i=0; i<14; i+=2) {
+    const addHrs = plusDays[i / 2].checked;
+    // first time in/out input of day
+    // don't include +1 day for the first time-in of each day
+    const time0 = timeType === 'in' ? 
+      convertTime(timeInputs[i].value, false) :
+      convertTime(timeInputs[i].value, addHrs);
+
+    // second time in/out input of day
+    const time1 = convertTime(timeInputs[i+1].value, addHrs);
+    output.push(time0);
+    output.push(time1);
   }
-  while(errMsgs.firstChild) {
-    errMsgs.removeChild(errMsgs.firstChild);
+  return output;
+}
+
+
+// CONVERT TIME INPUT STRINGS TO NUMBERS
+function convertTime(str, plusDay = false) {
+  // Add additional hours if +1 day is checked
+  const addHrs = plusDay ? 24 : 0;
+  let output;
+
+  if(str) {
+    const splitStr = str.split(':');
+    output = {};
+    output.hour = Number(splitStr[0]) + addHrs;
+    output.minutes = Number(splitStr[1]);
   }
-  errMsgs.classList.remove('show-errors')
-  window.scrollTo(0,0);
+  return ( output ? output : null);
+}
+
+
+// CALCULATE HOURS WORKED
+function calculateHrs(inTimes, outTimes) {
+  let totalWeek = 0
+  for (let i=0; i<14; i+=2) {
+    let totalDay = 0
+    if(inTimes[i])
+      totalDay = calcTimeWorked(inTimes[i], outTimes[i])
+
+    if(inTimes[i+1])
+      totalDay += calcTimeWorked(inTimes[i+1], outTimes[i+1]);
+
+    if (totalDay > 0) {
+      const totalHrs = Math.floor(totalDay / 60);
+      const totalMins = totalDay % 60;
+      document.querySelectorAll('.hours-worked')[i / 2].textContent = 
+      `${totalHrs}h${totalMins}m`
+    }
+    totalWeek += totalDay;
+  }
+  if (totalWeek > 0) {
+    const totalHrs = Math.floor(totalWeek / 60);
+    const totalMins = totalWeek % 60;
+    document.querySelector('.total-hours').textContent = 
+    `${totalHrs}h${totalMins}m`
+  }
+}
+
+function calcTimeWorked(inTime, outTime) {
+  const minutesIn = inTime.hour * 60 + inTime.minutes;
+  const minutesOut = outTime.hour * 60 + outTime.minutes;
+
+  return minutesOut - minutesIn;
+}
+
+const foo = {
+  hour: 8,
+  minutes: 50
+}
+const barr = {
+  hour: 10,
+  minutes: 10
 }
